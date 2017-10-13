4707378668bcefa2b15539bcdeb38ee6a7592369
Leonardo Souza <leonardocsouza@gmail.com>
2017-10-13 10:06:38 -0400

    add SNS topic publishing to lambda deploy function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,20 +4,30 @@ import boto3
 import mimetypes
 
 def handler(events, context):
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.leonardocsouza.com')
-    build_bucket = s3.Bucket('portfoliobuild.leonardocsouza.com')
+    # To notify of successfull deploy
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:737274848372:deployPortfolioTopic')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    try:
+        # To download from build bucket and unzip into portfolio bucket
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('portfolio.leonardocsouza.com')
+        build_bucket = s3.Bucket('portfoliobuild.leonardocsouza.com')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    print 'Job done!'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job done!'
+        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed!')
+    except:
+        topic.publish(Subject='Portfolio Deploy Failed', Message='It did not work')
+        raise
 
     return 'Completed lambda function'

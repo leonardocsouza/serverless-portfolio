2d89e51713a119458d6672dd0bed9c30023cae94
Leonardo Souza <leonardocsouza@gmail.com>
2017-10-13 11:09:53 -0400

    add codepipeline support to lambda deploy function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,14 +8,30 @@ def handler(events, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:737274848372:deployPortfolioTopic')
 
+    location = {
+        'bucketName': 'portfoliobuild.leonardocsouza.com',
+        'objectKey': 'portfoliobuild.zip'
+    }
+
     try:
         # To download from build bucket and unzip into portfolio bucket
+
+        # check if we received param through CodePipeline invocation
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == 'MyAppBuild':
+                    location = artifact['location']['s3Location']
+
+        print 'Building porfolio from', location
+
         s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('portfolio.leonardocsouza.com')
-        build_bucket = s3.Bucket('portfoliobuild.leonardocsouza.com')
+        build_bucket = s3.Bucket(location['bucketName'])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -26,8 +42,19 @@ def handler(events, context):
 
         print 'Job done!'
         topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed!')
+
+        # Notify that job was completed if it came from CodePipeline
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job['id'])
     except:
         topic.publish(Subject='Portfolio Deploy Failed', Message='It did not work')
+
+        # Notify that job failed if it came from CodePipeline
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_failure_result(jobId=job['id'],
+                failureDetails={'type':'JobFailed', 'message':'failed'})
         raise
 
     return 'Completed lambda function'
